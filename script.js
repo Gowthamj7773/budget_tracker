d2a97df860f93b1f582962116f8ef6e0f6eb144d
Gowthamj7773 <gowthamj7773@gmail.com>
2025-02-20 06:14:34 +0530

    modified js,style

--- script.js
+++ script.js
@@ -20,27 +20,32 @@ function transaction()
 
     //last values
     previous_val = amount.value
+    if(amount.value && description.value)
+        {
     if(amount.value.length<=15 && description.value.length<=15){
     
         if(Number(amount.value) >= 0)
         {
-        new_element.innerHTML = "<div class=\"right_list_data\" style=\"display:flex\">" +
+        new_element.innerHTML = "<div class=\"right_list_data\" id=\"right_list_data_income\">" +
                                     "<p style=\"padding:10px\">" + description.value + "</p>" +
-                                    "<p style=\"padding:10px;margin-left:50px;\">" + previous_val + "</p>" +
+                                    "<p style=\"padding:10px;margin-left:50px;\">"+"₹ "+ previous_val + "</p>" +
                                     "<button class=\"right_delete_button\" onclick=\"del(event)\"></button>" +
                                     "</div>";
-        //for appending
+
         right_list.append(new_element)
         main_balance+=Number(previous_val)
         income+=Number(previous_val)
         top_money.textContent = "₹ "+main_balance
         mid_money1.textContent = "₹ "+income
+
+        description.value=""
+        amount.value=""
         }
         else if(Number(amount.value) < 0)
             {
-            new_element.innerHTML = "<div class=\"right_list_data\" style=\"display:flex\">" +
+            new_element.innerHTML = "<div class=\"right_list_data\" id=\"right_list_data_expense\">" +
                                         "<p style=\"padding:10px\">" + description.value + "</p>" +
-                                        "<p style=\"padding:10px;margin-left:50px;\">" + previous_val + "</p>" +
+                                        "<p style=\"padding:10px;margin-left:50px;\">"+"₹ "+ previous_val + "</p>" +
                                         "<button class=\"right_delete_button\" onclick=\"del(event)\"></button>" +
                                         "</div>";
             right_list.append(new_element)
@@ -48,14 +53,23 @@ function transaction()
             expense+=Number(-1*previous_val)
             top_money.textContent = "₹ "+main_balance
             mid_money2.textContent = "₹ "+expense
+
+            description.value=""
+            amount.value=""
             }
     }
     else{
         alert("Too Long. please reduce the size of the input")
+        }
+    }
+    else{
+        alert("Please fill all the inputs")
     }
 } 
 
 function del(event)
 {
+    /*var main_b = main_balance-previous_val
+    console.log(main_b)*/
     event.target.parentElement.remove()
 }
